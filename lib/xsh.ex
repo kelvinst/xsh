6b5198428d68cf486f06b6a91d9f77cb951775fb
kelvinst <kelvin.stinghen@gmail.com>
2019-08-23 07:31:14 -0300

    Initial commit

---
+++ lib/xsh.ex
@@ -0,0 +1,18 @@
+defmodule XSH do
+  @moduledoc """
+  Documentation for XSH.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XSH.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xsh_test.exs
@@ -0,0 +1,8 @@
+defmodule XSHTest do
+  use ExUnit.Case
+  doctest XSH
+
+  test "greets the world" do
+    assert XSH.hello() == :world
+  end
+end
